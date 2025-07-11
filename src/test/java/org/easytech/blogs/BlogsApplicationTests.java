36ccf2519060d83646d4f81e30eac02111bb7b0d
unknown <minifun>
2025-07-11 14:40:50 +0800

    first commit

---
+++ src/test/java/org/easytech/blogs/BlogsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.easytech.blogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
